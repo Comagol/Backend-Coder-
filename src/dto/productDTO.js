72a4ffafb8a34dadc32ca17df737744f8f6ddcf9
Comagol <comabellafrancisco@gmail.com>
2025-08-21 23:47:55 -0300

    Add ProductDTO class for structured product data transfer

---
+++ src/dto/productDTO.js
@@ -0,0 +1,15 @@
+
+export class ProductDTO {
+  constructor(product) {
+    this.id = product._id;
+    this.title = product.title;
+    this.description = product.description;
+    this.code = product.code;
+    this.price = product.price;
+    this.stock = product.stock;
+    this.category = product.category;
+    this.thumbnail = product.thumbnail || [];
+  }
+
+  
+}
