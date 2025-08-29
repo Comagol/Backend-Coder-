f3803d8c864d728e2f8af1b850c3753389cf20f2
Comagol <comabellafrancisco@gmail.com>
2025-08-29 15:53:03 -0300

    Add index.js to export repository classes for Product, Cart, User, and Ticket management

---
+++ src/repositories/index.js
@@ -0,0 +1,4 @@
+export { ProductRepository } from './productRepository.js';
+export { CartRepository } from './cartRepository.js';
+export { UserRepository } from './userRepository.js';
+export { TicketRepository } from './ticketRepository.js';
